a5b76572cd5c07dae83fe42c051b7ec0c39cd242
Carlos Bellete <dcarlosbellete@gmail.com>
2024-01-14 23:53:44 -0300

    Estruturação de projeto

---
+++ BuscaCep.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1
+{
+    class BuscaCep : Begin
+    {
+        [Test]
+        public void Test1()
+        {
+            driver.FindElement(By.Id("endereco")).SendKeys("58415285");
+        }
+    }
+}
